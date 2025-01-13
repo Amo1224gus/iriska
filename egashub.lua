1047cc5696065483a6e105a8034f55d4894ca8fe
Ега_биб <130549159+Amo1224gus@users.noreply.github.com>
2025-01-13 22:57:35 +0300

    Update egashub.lua

--- egashub.lua
+++ egashub.lua
@@ -4,11 +4,9 @@ local localeId = player.LocaleId
 print("Текущий LocaleId:", localeId)
 
 if localeId == "ru-ru" then
-    print("Русский язык выбран. Запуск русского скрипта.")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/ru.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/ru.lua"))();
 elseif localeId == "uk-ua" then
-    print("Украинский язык выбран. Запуск украинского скрипта.")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/egashub.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/egashub.lua"))();
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/english.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/english.lua"))();
 end
