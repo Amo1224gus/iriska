f172f7d445fa31fdc2d9143990a1792390588ca8
Ега_биб <130549159+Amo1224gus@users.noreply.github.com>
2025-01-13 23:11:44 +0300

    Update egashub.lua

--- egashub.lua
+++ egashub.lua
@@ -5,7 +5,7 @@ print("Текущий LocaleId:", localeId)
 
 if localeId == "ru-ru" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/ru.lua"))();
-elseif localeId == "zh-CN" then
+elseif localeId == "pl-pl" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/egashub.lua"))();
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/english.lua"))();
