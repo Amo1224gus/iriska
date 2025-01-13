483ead7b2d7a64e2a4f8608a94d4fe1d650733c3
Ега_биб <130549159+Amo1224gus@users.noreply.github.com>
2025-01-13 22:53:03 +0300

    Update egashub.lua

--- egashub.lua
+++ egashub.lua
@@ -1,10 +1,10 @@
 local player = game.Players.LocalPlayer
 local localeId = player.LocaleId 
 
-if localeId == "ru" then
+if localeId == "ru-ru" then
     print("русский язык выбран")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/ru.lua"))();
-elseif localeId == "uk" then
+elseif localeId == "uk-ua" then
     print("Украинский язык выбран.")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Amo1224gus/iriska/refs/heads/main/uk.lua"))();
 else
